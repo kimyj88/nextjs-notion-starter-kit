a801f433eacaf44a25fbe6dcc57266d06d1c9460
kimyj88 <42383031+kimyj88@users.noreply.github.com>
2025-03-27 00:08:46 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '1c21d4fcf1e98004a227d5acb9c0cde8',
+  rootNotionPageId: '1701d4fcf1e980e4a0b8f2f54d23374a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -11,7 +11,7 @@ export default siteConfig({
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
   domain: 'nextjs-notion-starter-kit-seven-inky.vercel.app',
-  author: 'Travis Fischer',
+  author: 'Zaku',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
